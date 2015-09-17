e2e748a2a87ef27ce68c0a4a5787a967b46f3918
josemartin81 <jose_etsi@hotmail.com>
2015-09-17 12:30:14 +0200

    Homework #2. 3 pages designed (main, order & expedite)

--- RestaurantManager/RestaurantManager.UniversalWindows/RestaurantManager.UniversalWindows.Windows/OrderPage.xaml.cs
+++ RestaurantManager/RestaurantManager.UniversalWindows/RestaurantManager.UniversalWindows.Windows/OrderPage.xaml.cs
@@ -20,12 +20,11 @@ namespace RestaurantManager.UniversalWindows
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class OrderPage : Page
     {
-        public MainPage()
+        public OrderPage()
         {
             this.InitializeComponent();
         }
-
     }
 }
